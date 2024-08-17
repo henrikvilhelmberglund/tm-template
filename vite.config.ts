fbe34fa604d6c607e1547bff5625d5e3e71b1f49
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2024-08-17 13:15:41 +0200

    feat: create require file from package.json info

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import monkey from "vite-plugin-monkey";
 import UnoCSS from "unocss/vite";
+import { metadata } from "./metadata.ts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,11 +17,7 @@ export default defineConfig({
 				closePreviewAutomatically: true
 			},
 			userscript: {
-				icon: "https://vitejs.dev/logo.svg",
-				namespace: "npm/vite-plugin-monkey",
-				match: ["https://x.com/home"],
-				grant: "GM_xmlhttpRequest"
-				// connect: ["connectdomainshere.com"],
+				...metadata
 			}
 		})
 	]
